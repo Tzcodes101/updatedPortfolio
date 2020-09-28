6a2a7a62a3856d224dd65b338ab153cb127207bc
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2020-09-28 12:28:44 -0400

    push for ask bcs

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+$(".icon").on("click", function toggleNav() {
+    console.log("hi");
+    if (($("#my-links")).display = none) {
+        $("#my-links").removeId("my-links").setAttr("id", "display-my-links");
+    }
+});
+
+});
