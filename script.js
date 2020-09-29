e6506fa92c403c2ffb9addaaa9ebb9989dfe3d56
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2020-09-28 21:51:50 -0400

    resume addition

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 $(document).ready(function () {
 
+function hideLinks() {
+    $('#my-links').css('display', 'none');
+}
+
 $(".icon").on("click", function toggleNav() {
     console.log("hi");
-    if (($("#my-links")).display = none) {
-        $("#my-links").removeId("my-links").setAttr("id", "display-my-links");
+    if ($("#my-links").display === 'none') {
+        $('#my-links').css('display', 'block');
+    } else {
+        $('#my-links').css('display', 'none')
     }
 });
+
+hideLinks();
 
 });
