39a69429a3089db24c2c567d6c0eab11d059c338
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2020-09-29 21:36:19 -0400

    finished by nav menu

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 $(document).ready(function () {
 
 function hideLinks() {
-    $('#my-links').css('display', 'none');
+   if ($('#my-links').css('display', 'block')) {
+    ($("#my-links").display === 'none');
+   };
 }
 
 $(".icon").on("click", function toggleNav() {
     console.log("hi");
-    if ($("#my-links").display === 'none') {
         $('#my-links').css('display', 'block');
-    } else {
-        $('#my-links').css('display', 'none')
-    }
 });
 
 hideLinks();
